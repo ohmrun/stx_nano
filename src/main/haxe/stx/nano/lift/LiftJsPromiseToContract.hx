f6606c4f1b98879852f7c5e29768e60e58b2a98f
dayz <polysemantic@gmail.com>
2021-02-09 07:25:37 +0100

    aliasing of Promise

--- src/main/haxe/stx/nano/lift/LiftJsPromiseToContract.hx
+++ src/main/haxe/stx/nano/lift/LiftJsPromiseToContract.hx
@@ -2,7 +2,7 @@ package stx.nano.lift;
 
 class LiftJsPromiseToContract{
   #if js
-  static public function toContract<T,E>(self:Promise<T>):Contract<T,E>{
+  static public function toContract<T,E>(self:js.lib.Promise<T>):Contract<T,E>{
     return Contract.fromJsPromise(self);
   }
   #end
