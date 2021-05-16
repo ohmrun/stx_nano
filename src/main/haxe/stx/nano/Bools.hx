0f698809fbe90a5df57d1369cf66309ea96c636b
0b1kn00b <polysemantic@gmail.com>
2021-05-16 14:59:57 +0200

    StringTools doesn't like null

--- src/main/haxe/stx/nano/Bools.hx
+++ src/main/haxe/stx/nano/Bools.hx
@@ -2,7 +2,7 @@ package stx.nano;
 
 class Bools{
   static public function truthiness(str:String){
-    return switch(StringTools.trim(str)){
+    return switch(__.option(str).map(StringTools.trim).defv(null)){
       case "true"   : true;
       case "1"      : true;
       case "0"      : false;
