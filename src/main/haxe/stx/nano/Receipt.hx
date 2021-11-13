f975d9f8c82dc607e254bf022afb324568b0718e
0b1kn00b <polysemantic@gmail.com>
2021-11-13 08:48:59 +0100

    oh feck

---
+++ src/main/haxe/stx/nano/Receipt.hx
@@ -0,0 +1,78 @@
+package stx.nano;
+
+interface ReceiptApi<T,E> extends DefectApi<E>{
+  final value : Null<T>;
+}
+class ReceiptCls<T,E> extends DefectCls<E> implements ReceiptApi<T,E>{
+  public final value : Null<T>;
+  public function new(error,value:Null<T>){
+    super(error);
+    this.value = value;
+  }
+}
+typedef ReceiptDef<T,E> = DefectDef<E> & {
+  final value : Null<T>;
+}
+@:using(stx.nano.Receipt.ReceiptLift)
+@:forward abstract Receipt<T,E>(ReceiptDef<T,E>) from ReceiptDef<T,E> to ReceiptDef<T,E>{
+  static public var _(default,never) = ReceiptLift;
+  public function new(self) this = self;
+  @:noUsing static public function lift<T,E>(self:ReceiptDef<T,E>):Receipt<T,E> return new Receipt(self);
+  static public function unit<T,E>(){
+    return make(null,Iter.unit());
+  }
+  @:noUsing static public function make<T,E>(value:Null<T>,?error:Iter<E>){
+    return lift(new ReceiptCls(__.option(error).defv(Iter.unit()),value));
+  }
+  public function prj():ReceiptDef<T,E> return this;
+  private var self(get,never):Receipt<T,E>;
+  private function get_self():Receipt<T,E> return lift(this);
+
+  @:noUsing static public function fromDefect<T,E>(self:Defect<E>):Receipt<T,E>{
+    return make(null,self.error);
+  }
+  @:noUsing static public function pure<T,E>(self:T):Receipt<T,E>{
+    return make(self,Iter.unit());
+  }
+  @:to public function toError(){
+    return new stx.nano.error.term.DefectError(this.error).toError();
+  }
+}
+class ReceiptLift extends Clazz{
+  static public function make(){
+    return new ReceiptLift();
+  }
+  static public function lift<T,E>(self:ReceiptDef<T,E>):Receipt<T,E>{
+    return Receipt.lift(self);
+  }
+  static public function errate<T,E,EE>(self:ReceiptDef<T,E>,fn:E->EE):Receipt<T,EE>{
+    return errata(self,x -> x.map(fn));
+  }
+  static public function errata<T,E,EE>(self:ReceiptDef<T,E>,fn:Error<E>->Error<EE>){
+    return Receipt.make(
+      self.value,
+      fn(new stx.nano.error.term.DefectError(self.error)).content()
+    );
+  }
+  static public function copy<T,E>(self:ReceiptDef<T,E>,?value:T,?error:Iter<E>){
+    return Receipt.make(
+      __.option(value).defv(self.value),
+      __.option(error).defv(self.error)
+    );
+  }
+  static public function map<T,Ti,E>(self:ReceiptDef<T,E>,fn:T->Ti):Receipt<Ti,E>{
+    return Receipt.make(
+      __.option(self.value).fold(
+        ok -> fn(ok),
+        () -> null
+      ),
+      self.error
+    );
+  }
+  static public function is_defined<T,E>(self:ReceiptDef<T,E>){
+    return self.value != null;
+  }
+  static public function has_errors<T,E>(self:ReceiptDef<T,E>){
+    return self.error.is_defined();
+  }
+}
---
+++ src/main/haxe/stx/nano/error/term/DefectError.hx
@@ -0,0 +1,41 @@
+package stx.nano.error.term;
+
+class DefectError<E> extends stx.pico.error.term.ErrorBase<E> implements DefectApi<E>{
+  public var error(get,null) : Iter<E>;
+  public function get_error():Iter<E>{
+    return this.error;
+  }
+  
+  public function new(error:Iter<E>){
+    super();
+    this.error = error;
+  }
+  public function get_pos(): Option<Pos>{
+    return None;
+  }
+  public function get_val() : Option<E>{
+    return this.error.head();
+  }
+  public function get_lst() : Option<Error<E>>{
+    return Some(new DefectError(error.tail()).toError());
+  }
+  public function concat(that:Error<E>){
+    return switch(that.pos){
+      case Some(e) : new stx.pico.error.term.ErrorConcat(that,this);
+      case None    : new DefectError(
+        that.val.map(
+          (x:E) -> 
+            that.rest()
+                .map_filter((y:Error<E>) -> y.val)
+                .snoc(x))
+                .map(this.error.concat).defv(this.error)
+      ).toError();
+    }
+  }
+  public function copy(){
+    return new DefectError(this.error).toError();
+  }
+  public function toDefect(){
+    return this;
+  }
+}
