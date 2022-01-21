e5c3e91f8f63db70c305fe1a08f5c8e97d3695c3
0b1kn00b <polysemantic@gmail.com>
2022-01-21 15:14:55 +0100

    Retry

--- src/main/haxe/stx/nano/Retry.hx
+++ src/main/haxe/stx/nano/Retry.hx
@@ -2,16 +2,32 @@ package stx.nano;
 
 class Retry{
   public final attempts : Int;
-  public final duration : Float;
+  public final born     : Float;
 
-  public function new(attempts,duration){
+  public function new(attempts,born){
     this.attempts = attempts;
-    this.duration = duration;
+    this.born     = born;
   }
-  public function copy(?attempts,?duration){
+  public function copy(?attempts,?born){
     return new Retry(
       __.option(attempts).defv(this.attempts),
-      __.option(duration).defv(this.duration)
+      __.option(born).defv(this.born)
     );
   }
+  static public function make(attempts,born){
+    return new Retry(attempts,born);
+  }
+  static public function unit(){
+    return make(0,haxe.Timer.stamp());
+  }
+  public var duration(get,never) : Float;
+  public function get_duration(){
+    return haxe.Timer.stamp() - this.born;
+  }
+  public function next(){
+    return copy(this.attempts + 1);
+  }
+  // public function gt(that:Retry){
+  //   return this.attempts > 
+  // }
 }
